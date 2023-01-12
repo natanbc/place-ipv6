d085e665b6e18b6ca11a34df2d61a921a909e990
natanbc <natanbc@users.noreply.github.com>
2023-01-12 23:59:22 +0000

    performance improvements

--- icmp.py
+++ icmp.py
@@ -1,6 +1,7 @@
 import argparse
 import asyncio
 import imageio
+import numpy as np
 from queue import Queue
 import socket
 import struct
@@ -15,22 +16,21 @@ image = imageio.imread(args.image, pilmode="RGBA")
 w, h, _ = image.shape
 if w != 512 or h != 512:
     raise Exception("Image is not 512x512")
-r, g, b = image[:, :, 0], image[:, :, 1], image[:, :, 2]
-
+img_r, img_g, img_b = image[:, :, 0], image[:, :, 1], image[:, :, 2]
 
 sock = socket.socket(socket.AF_INET6, socket.SOCK_RAW, socket.IPPROTO_ICMPV6)
 data = b"\x08" + b"\x00" * 7
-base_ip = b"\x2a\x06\xa0\x03\xd0\x40"
-def make_ip(x, y, r, g, b):
-    return base_ip + struct.pack("BBBBxBxBxB", 
-        0x10 | (x >> 8),
-        x & 0xFF,
-        y >> 8,
-        y & 0xFF,
-        r,
-        g,
-        b,
-    )
+
+_ip_buf = bytearray(socket.inet_pton(socket.AF_INET6, "2a06:a003:d040::"))
+def make_ip(x, y, r, g, b, size=1):
+    _ip_buf[6] = (size << 4) | (x >> 8)
+    _ip_buf[7] = x & 0xFF
+    _ip_buf[8] = y >> 8
+    _ip_buf[9] = y & 0xFF
+    _ip_buf[11] = r
+    _ip_buf[13] = g
+    _ip_buf[15] = b
+    return _ip_buf
 
 class CanvasHolder:
     def __init__(self):
@@ -41,13 +41,8 @@ class CanvasHolder:
         self.q.put(img)
     
     def _merge(self, img):
-        for y in range(512):
-            row = img[y]
-            if not row.any():
-                continue
-            for x in range(512):
-                if row[x, 3] != 0:
-                    self.canvas[y, x] = row[x]
+        nz = np.nonzero(img[:, :, 3])
+        self.canvas[nz] = img[nz]
 
     def get_canvas(self):
         if self.q.empty() and self.canvas is not None:
@@ -77,20 +72,24 @@ threading.Thread(target=asyncio_thread, args=(asyncio.get_event_loop(),)).start(
 
 def paint():
     canvas = ch.get_canvas()
+    canvas_r, canvas_g, canvas_b = canvas[:, :, 0], canvas[:, :, 1], canvas[:, :, 2]
     painted = 0
     for y in range(512):
         if y % 64 == 0:
             print("Y =", y)
+        row_r, row_g, row_b = img_r[y], img_g[y], img_b[y]
+        c_row_r, c_row_g, c_row_b = canvas_r[y], canvas_g[y], canvas_b[y]
         for x in range(512):
-            if r[y, x] == canvas[y, x, 0] and g[y, x] == canvas[y, x, 1] and b[y, x] == canvas[y, x, 2]:
+            r, g, b = row_r[x], row_g[x], row_b[x]
+            if r == c_row_r[x] and g == c_row_g[x] and b == c_row_b[x]:
                 continue
             painted += 1
             ip = socket.inet_ntop(socket.AF_INET6, make_ip(
                 x,
                 y,
-                r[y, x],
-                g[y, x],
-                b[y, x]
+                r,
+                g,
+                b,
             ))
             sock.sendto(data, (ip, 0, 0, 0))
             sock.sendto(data, (ip, 0, 0, 0))
@@ -98,4 +97,3 @@ def paint():
 
 while True:
     paint()
-    time.sleep(1)
